dec656a52b53397658fd45ddbc543f0605484701
aaron saunders <aaronksaunders@gmail.com>
2024-04-22 00:39:02 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import tw from 'twrnc';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={tw.style('text-2xl bg-blue-500 p-8')}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
